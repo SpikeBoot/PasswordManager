2d2bffdf84d11ad411a17d7497d3f1ab4cce9591
SpikeBoot <84537377+SpikeBoot@users.noreply.github.com>
2022-01-20 18:53:43 +0200

    add DBWorker for working with SQLite and create DB file

---
+++ src/main/java/core/DBWorker.java
@@ -0,0 +1,39 @@
+package core;
+
+import java.sql.*;
+
+public class DBWorker {
+    private final String URL;
+
+    Connection connection;
+
+    DBWorker (String url){
+        this.URL = url;
+
+        try{
+            Class.forName("org.sqlite.JDBC");
+            System.out.println("Driver was register");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection(){
+        try{
+            connection = DriverManager.getConnection(URL);
+            System.out.println("Connection is fine");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
+
+class Test{
+    public static void main(String[] args) {
+        DBWorker db = new DBWorker("jdbc:sqlite:password_base.db");
+        db.getConnection();
+    }
+}
